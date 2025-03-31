ec74b8778d9710727ca403fae6c8503a39d22b60
Elefave <efavelukes@mail.austral.edu.ar>
2025-03-31 12:08:52 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -4,10 +4,10 @@ def ada():
     full_name = first_name + " " + last_name
     lower_name = full_name.lower()
     print(lower_name)
-    upper_name = full_name.upper()
-    print(upper_name)
     capital_name = full_name.title()
     print(capital_name)
+    upper_name = full_name.upper()
+    print(upper_name)
     print("       " + lower_name)
 
 
